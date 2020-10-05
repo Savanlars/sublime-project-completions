013cd4d63174f2a012d5d29a7c6184447c6cb27b
Megalobit <71697802+Savanlars@users.noreply.github.com>
2020-10-05 14:38:37 +0500

    Доработка: добавлена обработка списка из нескольких элементов, как в словаре так и в списке
    Теперь список блока автокомплита (в файле проекта), может содержать более чем два элемента.
    Т.е. длинную строку кода можно разбить на любое количество подстрок, что бывает удобно.
    
    Примеры:
    
    "completions": [
        [trigger, completion],
        ...
        [trigger, 
        completion_Part1,
        completion_Part2,
        completion_Part3]
    ]
    
    или
    
    "completions": {
        selector: [             
            [trigger, completion]
            ...
            [trigger, 
             completion_Part1, 
             completion_Part2, 
             completion_Part3],
        ],

--- ProjectCompletions.py
+++ ProjectCompletions.py
@@ -1,31 +1,32 @@
 import sublime_plugin
 
-def all_match(view, locs, selector):
-    for loc in locs:
-        if selector != view.scope_name(loc).rstrip():
-            return False
-    return True
-
 class ProjectCompletions(sublime_plugin.EventListener):
 
     def on_query_completions(self, view, prefix, locations):
         if view.window() and view.window().project_data():
             completions = view.window().project_data().get("completions")
-            if isinstance(completions, list):
-                return completions
 
+            if isinstance(completions, list):
+                return parse_completions(completions)
             elif isinstance(completions, dict):
                 result = []
                 for selector in completions:
                     if all_match(view, locations, selector):
-
-                        parse_completions = []
-                        for item in completions[selector]:
-                            if len(item) > 1:
-                                item = [item[0], ''.join(item[1:])]
-                                parse_completions += [item]
-
-                        result += parse_completions
-
+                        result += parse_completions(completions[selector])
                 return result
+
         return None
+
+def parse_completions(completions):
+    result = []
+    for item in completions:
+        if len(item) > 1:
+            item = [item[0], ''.join(item[1:])]
+            result += [item]
+    return result
+
+def all_match(view, locs, selector):
+    for loc in locs:
+        if selector != view.scope_name(loc).rstrip():
+            return False
+    return True
